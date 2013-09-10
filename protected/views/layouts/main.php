d19cc97dc100fa2866caae6a3da39c4cd8284108
Dimitar Dimitrov <me@ddimitrov.name>
2013-09-10 13:03:09 +0300

    Translate a string in the main layout

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -60,7 +60,7 @@
 		<nav class="navbar navbar-inverse" role="navigation">
 			<div class="navbar-header">
 				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-					<span class="sr-only">Toggle navigation</span>
+					<span class="sr-only">Навигация</span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
