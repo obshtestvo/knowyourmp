f2deee7d94ec36a2204f5af84a3bdc064ce1d2d9
Dimitar Dimitrov <me@ddimitrov.name>
2016-12-03 12:15:59 +0200

    Add Google Analytics tracking to the site

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -19,7 +19,7 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 
 	<!-- project styles -->
 	<link rel="stylesheet" href="/css/main.css" />
-	
+
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 
@@ -58,6 +58,15 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 /mobile/i.test(navigator.userAgent) && !window.location.hash && setTimeout(function () {
   window.scrollTo(0, 1);
 }, 200);
+</script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-88347965-1', 'auto');
+  ga('send', 'pageview');
 </script>
 </body>
 </html>
